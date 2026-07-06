e604775c7827793dcc467bb3d8c96e7b993340cc
Anna Seo <hyunjiseo@Anna-Mac.local>
2026-07-06 15:32:48 -0500

    add qualify to session conversions

--- models/intermediate/thelook_ecommerce/int_session_conversions.sql
+++ models/intermediate/thelook_ecommerce/int_session_conversions.sql
@@ -2,6 +2,8 @@ with purchased_events as (
     select
         session_id,
         user_id,
+        traffic_source,
+        browser,
         created_at,
         event_type
     from {{ ref('stg_events') }}
@@ -12,7 +14,9 @@ final as (
 
     select
         session_id,
-        max(user_id) as user_id,
+        user_id,
+        traffic_source,
+        browser,
         min(created_at) as session_start,
         max(created_at) as session_end,
         count(*) as total_events,
@@ -22,7 +26,12 @@ final as (
         countif(event_type = 'purchase') as purchase_events,
         max(if(event_type = 'purchase', 1, 0)) as has_purchased
     from purchased_events
-    group by session_id
+    group by session_id, user_id, traffic_source, browser
+
+    qualify row_number() over(
+        partition by session_id
+        order by min(created_at) asc
+    ) = 1
 
 )
 
